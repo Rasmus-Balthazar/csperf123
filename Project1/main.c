df6cf9d65a3c80ae8fcf3b1a54e5407e7fffaccc
Michael Jeppesen <micje@itu.dk>
2025-02-25 10:44:05 +0100

    Seperated main logic into methods

--- Project1/main.c
+++ Project1/main.c
@@ -8,14 +8,34 @@
 #define DATA_PER_PARTITION (NUM_PARTITIONS * PARTITION_SIZE)
 #define SAMPLE_SIZE (DATA_PER_PARTITION * NUM_THREADS)
 
+Tuple ****run_independent(Tuple **data);
+Tuple ***run_ctm(Tuple **data);
+void print_partition(Tuple **partition, int partition_size);
+
 int main(int argc, char **argv) {
     Tuple **data = gen_data(SAMPLE_SIZE);
-    // Tuple ****independently_partitioned = partition_independent(SAMPLE_SIZE, data, NUM_THREADS, NUM_PARTITIONS, PARTITION_SIZE);
-    Tuple ***count_then_move = count_then_move_partition(SAMPLE_SIZE, data, NUM_THREADS, NUM_PARTITIONS);
-    for (int i = 0; i < 32; i++)
-    {
-        printf("%lu\n", (*(*(count_then_move)+i))->partitionKey);
-        // printf("%lu\n", (*(*(*(independently_partitioned))+i))->partitionKey);
-    }
+
+    // Tuple ***count_then_move = run_ctm(data);
+    Tuple ****independently_partitioned = run_independent(data);
+
+    print_partition(independently_partitioned[0][0], 16);
+
     return 0;
+}
+
+Tuple ****run_independent(Tuple **data) {
+    Tuple ****independently_partitioned = partition_independent(SAMPLE_SIZE, data, NUM_THREADS, NUM_PARTITIONS, PARTITION_SIZE);
+    return independently_partitioned;
+}
+
+Tuple ***run_ctm(Tuple **data) {
+    Tuple ***count_then_move = count_then_move_partition(SAMPLE_SIZE, data, NUM_THREADS, NUM_PARTITIONS);
+    return count_then_move;
+}
+
+void print_partition(Tuple **partition, int partition_size) {
+    for (int i = 0; i < partition_size; i++)
+    {
+        printf("%lu\n", partition[i]->partitionKey);
+    }
 }
