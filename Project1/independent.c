0e199486c18d9e497af3ea2c7078e17c5c2d6f00
Michael Jeppesen <mj@lpj-software.dk>
2025-03-03 19:07:55 +0100

    Fixed problem with incorrectly calculated partition size in independent

--- Project1/independent.c
+++ Project1/independent.c
@@ -5,7 +5,7 @@ Tuple ****partition_independent(int data_size, Tuple **data, int num_threads, in
     // Tuple **data = gen_data(SAMPLE_SIZE);
     Tuple ****buffers = (Tuple****)calloc(num_threads, sizeof(Tuple***));
     for (int i = 0; i < num_threads; i++) {
-        *(buffers + i) = (Tuple***)calloc(num_partitions, sizeof(Tuple**));
+        buffers[i] = (Tuple***)calloc(num_partitions, sizeof(Tuple**));
     }
 
     pthread_t threads[num_threads];
@@ -19,7 +19,7 @@ Tuple ****partition_independent(int data_size, Tuple **data, int num_threads, in
         if (args->endIndex >= data_size)
             args->endIndex = data_size - 1;
         args->data = data;
-        args->partitions = *(buffers + i);
+        args->partitions = buffers[i];
         args->numPartitions = num_partitions;
         args->partitionSize = partition_size;
 
@@ -41,16 +41,17 @@ int hash_key(uint64_t key, int num_partitions) {
 
 void *run(void *args) {
     Args *input = (Args *)args;
-    for(int i =  0; i < input->numPartitions; i++) {
+    for (int i = 0; i < input->numPartitions; i++)
+    {
         //Allocate every partition from null to a partion of the correct size.
         *(input->partitions+i) = (Tuple**)calloc(input->partitionSize << 1, sizeof(Tuple*));
     }
     int *offset = (int*)calloc(input->numPartitions, sizeof(int));
-    for (int i = input->startIndex; i < input->endIndex; i++) {
-        Tuple *data = *(input->data + i);
+    for (uint64_t i = input->startIndex; i < input->endIndex; i++) {
+        Tuple *data = input->data[i];
         int hashedKey = hash_key(data->partitionKey, input->numPartitions);
-        *(*(input->partitions + hashedKey) + *(offset + hashedKey)) = data;
-        (*(offset + hashedKey))++;
+        input->partitions[hashedKey][offset[hashedKey]] = data;
+        offset[hashedKey]++;
     }
     return 0;
 }
