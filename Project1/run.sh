78cf2ecb69905082dec1fb7580ed08637a33e722
Michael Jeppesen <mj@lpj-software.dk>
2025-03-04 14:04:21 +0100

    updated run script to have all the relevant events, and still work

--- Project1/run.sh
+++ Project1/run.sh
@@ -29,19 +29,16 @@ for keybits in {1..18}; do
     for threads in {1,2,4,8,16,32}; do
     # for threads in 2; do
         for i in {1..10}; do
-            perf stat -e \
-            branch-misses,iTLB-load-misses,dTLB-load-misses -o "./out/ctm/perf_ctm-${keybits}-${threads}-${i}.txt" ./main.exe $threads $keybits $tuplespower -a ctm -i $i >> ./out/data_ctm.txt
+            perf stat -e context-switches,cycles,instructions,L1-icache-load-misses,L1-dcache-load-misses,LLC-load-misses,cache-misses,uops_retired.stall_cycles,branch-misses,iTLB-load-misses,dTLB-load-misses -o "./out/ctm/perf_ctm-${keybits}-${threads}-${i}.txt" ./main.exe $threads $keybits $tuplespower -a ctm -i $i >> ./out/data_ctm.txt
             # ./main.exe $threads $keybits $tuplespower -a ctm >> data_ctm.txt
         done
         for i in {1..10}; do
-            perf stat -e  \
-            branch-misses,iTLB-load-misses,dTLB-load-misses -o "./out/independent/perf_independent-${keybits}-${threads}-${i}.txt" ./main.exe $threads $keybits $tuplespower -a i -i $i >> ./out/data_independent.txt
+            perf stat -e context-switches,cycles,instructions,L1-icache-load-misses,L1-dcache-load-misses,LLC-load-misses,cache-misses,uops_retired.stall_cycles,branch-misses,iTLB-load-misses,dTLB-load-misses -o "./out/independent/perf_independent-${keybits}-${threads}-${i}.txt" ./main.exe $threads $keybits $tuplespower -a i -i $i >> ./out/data_independent.txt
             # ./main.exe $threads $keybits $tuplespower -a i >> data_independent.txt
         done
     done
 done
 for i in {1..100}; do
-    perf stat -e  \
-    branch-misses,iTLB-load-misses,dTLB-load-misses -o "./out/dry/perf_dry-${i}.txt" ./main.exe 1 1 $tuplespower -i $i >> ./out/data_dry.txt
+    perf stat -e  context-switches,cycles,instructions,L1-icache-load-misses,L1-dcache-load-misses,LLC-load-misses,cache-misses,uops_retired.stall_cycles,branch-misses,iTLB-load-misses,dTLB-load-misses -o "./out/dry/perf_dry-${i}.txt" ./main.exe 1 1 $tuplespower -i $i >> ./out/data_dry.txt
     # ./main.exe $threads $keybits $tuplespower >> data_dry.txt
 done
