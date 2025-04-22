01b95ea085f7fb3daded6f8adc8851b6f907f7a5
Rasmus Balthazar Rødgaard Andreasen <rban@itu.dk>
2025-04-22 14:08:09 +0200

    Added initial pattern matching
    Co-authored-by: Dea <ThatOne2@users.noreply.github.com>
    Co-authored-by: Michael <micje@itu.dk>

--- Project2/SimpleRE.cu
+++ Project2/SimpleRE.cu
@@ -1,3 +1,4 @@
+#include <cuda_runtime.h>
 
 // Input, how much is left of input, Pattern, Pattern length
 /** Progression
@@ -5,6 +6,28 @@
  * Begin adding rules - only extract relevant RE patterns as Literal Search
  * Wildcards
  * Repetitions
- * Ranges/Sets
+ * Ranges/Sets 
  * Ors/Options
  */
+
+ /**
+  * How you doing? 
+  */
+
+  __global__ void simple_gpu_re(char *text, int text_len, char **patterns, int[] patterns_len) {
+    char *pattern = patterns+blockIdx.x;
+    int pattern_len = patterns_len[blockIdx.x];
+    int stride = blockDim.x;
+    for (int i = threadIdx.x; i < text_len; i += stride)
+    {
+        int pattern_off = 0;
+        int text_off = 0;
+        while (matches(pattern[pattern_off], text[i+text_off])) {
+            pattern_off++;
+            text_off++;
+            if (pattern_off > pattern_len)
+                break;
+        }
+    }
+  }
+  
