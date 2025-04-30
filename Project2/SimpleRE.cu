b51816c3309c073a51a50d4506a499343c8da7cd
Michael Jeppesen <mj@lpj-software.dk>
2025-04-30 12:35:19 +0200

    Added entrypoint running very simple linear search
    Co-authored-by: Dea <ThatOne2@users.noreply.github.com>

--- Project2/SimpleRE.cu
+++ Project2/SimpleRE.cu
@@ -1,5 +1,7 @@
 #include <stdbool.h>
+#include <string.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
 
 // Input, how much is left of input, Pattern, Pattern length
 /** Progression
@@ -23,11 +25,11 @@ typedef struct {
 
 bool matches(char pattern, char text);
 
-__global__ void simple_gpu_re(char *text, int text_len, int pattern_count, char **patterns, int patterns_len[], unsigned int matches_found[], Match match_arr[]) {
+__global__ void simple_gpu_re(char *text, int text_len, int pattern_count, char *patterns[], int patterns_len[], unsigned int matches_found[], Match match_arr[]) {
     int pattern_len = patterns_len[blockIdx.x];
     int stride = blockDim.x;
     for (int pattern_index = blockIdx.x; pattern_index < gridDim.x; pattern_index += gridDim.x) {
-        char *pattern = patterns + pattern_index;
+        char *pattern = patterns[pattern_index];
         for (int i = threadIdx.x; i < text_len; i += stride) {
             int pattern_off = 0;
             int text_off = 0;
@@ -47,28 +49,68 @@ __global__ void simple_gpu_re(char *text, int text_len, int pattern_count, char 
             }
             if ((i + stride) > matches_found[pattern_index] || (i+stride) > text_len) 
             {
+                // If match here, collection process can start,
                 __syncthreads(); // Synchronize threads in the block
                 if (threadIdx.x == matches_found[pattern_index]%stride) {
-                    match_arr[pattern_index] = {
-                        start_index = i,
-                        length = text_off,
-                        pattern_idx = pattern_index
-                    };
+                    Match* match = (Match*)calloc(1, sizeof(Match));
+                    match->start_index = i;
+                    match->length = text_off;
+                    match->pattern_idx = pattern_index;
+                    match_arr[pattern_index] = *match;
+
+                    printf("Match for pattern \"%s\" found at %i\n", patterns[pattern_index], i);
                 }
                 break;
             }
-                // If match here, collection process can start,
-            
         }
     }
-    
-    // collect within block to find first position of pattern matched
-
-    // Collect accross blocks, whith the match for each pattern
 }
 
 // Update this to work with tokens, and return how much of text was consumed
-bool matches(char pattern, char text)
+__device__ bool matches(char pattern, char text)
 {
     return pattern == text;
+}
+
+#define BLOCK_SIZE 256  // Number of threads per block
+#define ARRAY_SIZE 1024  // Size of the input arrays
+
+int main() {
+    //h_ for host 
+    char* h_text = "dette er en lang test tekst xD";
+    int text_len = strlen(h_text);
+    char* h_patterns[] = { "test", "er", "nope" };
+    int h_pattern_lens[] = {5, 3, 5}; //because of terminating char
+    unsigned int h_matches_found[] = {-1u, -1u, -1u};
+    Match* h_match_arr = (Match*)calloc(3, sizeof(Match)); 
+
+
+    // Device data allocation
+    // d_ for device 
+    char* d_text;
+    char** d_patterns;
+    int* d_pattern_lengths;
+    unsigned int* d_matches_found;
+    Match* d_match_arr;
+
+    cudaMalloc((void **)&d_text, text_len * sizeof(char));
+    cudaMalloc((void **)&d_patterns, (5+3+5)*sizeof(char));
+    cudaMalloc((void **)&d_pattern_lengths, 3*sizeof(int));
+    cudaMalloc((void **)&d_matches_found, 3*sizeof(unsigned int));
+    cudaMalloc((void **)&d_match_arr, 3*sizeof(Match));
+
+    // Copy input arrays to device
+    cudaMemcpy(d_text, h_text, text_len * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_patterns, h_patterns, (5+3+5)*sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_pattern_lengths, h_pattern_lens, 3*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_matches_found, h_matches_found, 3*sizeof(unsigned int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_match_arr, h_match_arr, 3*sizeof(Match), cudaMemcpyHostToDevice);
+
+
+    dim3 threadsPerBlock(BLOCK_SIZE);
+    dim3 blocksPerGrid((ARRAY_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE);
+
+    simple_gpu_re<<<blocksPerGrid, threadsPerBlock>>>(d_text, text_len, 3, d_patterns, d_pattern_lengths, d_matches_found, d_match_arr);
+
+    cudaMemcpy(h_match_arr, d_match_arr, 3*sizeof(Match), cudaMemcpyDeviceToHost);
 }
