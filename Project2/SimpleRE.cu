1b8539461241f13fac9eed7bc3955ae54935893c
Michael Jeppesen <mj@lpj-software.dk>
2025-04-30 11:09:31 +0200

    Various fixes for our linearsearch match
    Co-authored-by: Dea <ThatOne2@users.noreply.github.com>

--- Project2/SimpleRE.cu
+++ Project2/SimpleRE.cu
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <cuda_runtime.h>
 
 // Input, how much is left of input, Pattern, Pattern length
@@ -14,48 +15,51 @@
  * How you doing?
  */
 
-typdef struct
-{
-    int start_index, length;
+typedef struct {
+    int start_index;
+    int length;
     int pattern_idx;
-} match;
+} Match;
 
 bool matches(char pattern, char text);
 
-__global__ void simple_gpu_re(char *text, int text_len, int pattern_count, char **patterns, int[] patterns_len, unsigned int[] matches_found, match[] match_arr)
-{
-    char *pattern = patterns + pattern_index;
+__global__ void simple_gpu_re(char *text, int text_len, int pattern_count, char **patterns, int patterns_len[], unsigned int matches_found[], Match match_arr[]) {
     int pattern_len = patterns_len[blockIdx.x];
     int stride = blockDim.x;
-    for (int pattern_index = blockIdx.x; pattern_index < gridDim.x; pattern_index += gridDim.x)
-    {
-        for (int i = threadIdx.x; i < text_len; i += stride)
-        {
+    for (int pattern_index = blockIdx.x; pattern_index < gridDim.x; pattern_index += gridDim.x) {
+        char *pattern = patterns + pattern_index;
+        for (int i = threadIdx.x; i < text_len; i += stride) {
             int pattern_off = 0;
             int text_off = 0;
-            while (matches(pattern[pattern_off], text[i + text_off]))
-            {
+            while (matches(pattern[pattern_off], text[i + text_off])) {
                 pattern_off++;
                 text_off++;
                 // If the offset is longer than the pattern length we have found it
-                if (pattern_off > pattern_len)
-                {
-                    uint val = matches_found[pattern_index]
-                        // We are relying on the checks not being exhaustive by doing val > i before atomicCAS
-                        while (val > i && atomicCAS(matches_found + pattern_index, val, i) > i)
-                    {
+                if (pattern_off > pattern_len) {
+                    unsigned int val = matches_found[pattern_index];
+                    // We are relying on the checks not being exhaustive by doing val > i before atomicCAS
+                    while (val > i && atomicCAS(matches_found + pattern_index, val, i) > i) {
                         val = matches_found[pattern_index];
                         // Compares b to a, and if true then writes c into a.
                     }
                     break;
                 }
             }
-            if ((i + stride) > matches_found[pattern_index])
+            if ((i + stride) > matches_found[pattern_index] || (i+stride) > text_len) 
+            {
+                __syncthreads(); // Synchronize threads in the block
+                if (threadIdx.x == matches_found[pattern_index]%stride) {
+                    match_arr[pattern_index] = {
+                        start_index = i,
+                        length = text_off,
+                        pattern_idx = pattern_index
+                    };
+                }
                 break;
+            }
                 // If match here, collection process can start,
             
         }
-        __syncthreads(); // Synchronize threads in the block
     }
     
     // collect within block to find first position of pattern matched
