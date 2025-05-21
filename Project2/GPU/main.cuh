2703fbdcbb6b12953ac0cacca98a69eb659f0ad4
Rasmus Balthazar Andreasen <rban@itu.dk>
2025-05-21 12:28:37 +0200

    added initial timestamps for test compilation

--- Project2/GPU/main.cuh
+++ Project2/GPU/main.cuh
@@ -4,5 +4,10 @@
 #include <cuda_runtime.h>
 #include "preprocessing.cuh"
 #include "device.cuh"
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cuda_runtime.h>
 
 #endif
