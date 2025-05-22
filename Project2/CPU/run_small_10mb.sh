3c0a30da867ae4138e64c8ead575227bb1466680
Rasmus Balthazar Andreasen <rban@itu.dk>
2025-05-22 18:49:54 +0200

    Made output names more compact

--- Project2/CPU/run_small_10mb.sh
+++ Project2/CPU/run_small_10mb.sh
@@ -8,8 +8,8 @@ for input in "${inputs[@]}"; do
     input_name=$(basename "$input" .regex)
     
     for iteration in {1}; do
-        output_file="output_${input_name}_small.txt"
-        output_perf_file="output_perf_${input_name}_small.txt"
+        output_file="perf_${input_name}.txt"
+        output_perf_file="perf_${input_name}.txt"
 
         perf stat -o "$output_perf_file" -e cycles,instructions ./a.out "../Homemade_datasets/dataset_10mb.txt" "$input" > "$output_file"
  
